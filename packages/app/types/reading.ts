28c32c09765014da2aa6a4050bf85530943335d1
thewronghand <penfreak77@gmail.com>
2026-03-10 15:10:22 +0900

    feat: ReadingInterpretation 구조화 타입 정의

--- packages/app/types/reading.ts
+++ packages/app/types/reading.ts
@@ -1,6 +1,19 @@
 import { DrawnTarotCard } from './card'
 import { SpreadType } from './spread'
 
+export interface CardReading {
+  cardName: string
+  position: string
+  interpretation: string
+}
+
+export interface ReadingInterpretation {
+  cardReadings: CardReading[]
+  content: string
+  title: string
+  summary: string
+}
+
 export interface TarotReading {
   id: string
   spreadType: SpreadType
